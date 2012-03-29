c7501f680e53f52f993bbd9d6b25c61a0f8bc05d
Ben L. Titzer <titzer@google.com>
2012-03-28 21:23:05 -0700

    Add CiRuntime.callerIp() and CiRuntime.callerSp(); use to implement stacktrace for System.error on x86-darwin

--- test/rt/test.bash
+++ test/rt/test.bash
@@ -14,7 +14,7 @@ if [ "$TEST_TARGET" != "$target" ]; then
 fi
 
 printf "  Compiling ($target) RiRuntimeTest..."
-run_v3c $target $SOURCES &> $OUT/$target.compile.out
+run_v3c $target -output=$OUT $SOURCES &> $OUT/$target.compile.out
 check_no_red $? $OUT/$target.compile.out
 
 printf "  Compiling ($target-rt) RiRuntimeTest..."
@@ -24,3 +24,14 @@ check_no_red $? $OUT/$target-rt.compile.out
 printf "  Compiling ($target-gc) RiRuntimeTest..."
 run_v3c "" -target=$target -output=$OUT -heap-size=1k -rt.gc -rt.gctables -rt.sttables $SOURCES $RT/darwin/*.v3 $RT/native/*.v3 $RT/gc/*.v3 &> $OUT/$target-gc.compile.out
 check_no_red $? $OUT/$target-gc.compile.out
+
+printf "  Compiling ($target) CiRuntimeApi..."
+run_v3c $target -output=$OUT CiRuntimeApi.v3 &> $OUT/$target.compile.out
+check_no_red $? $OUT/$target.compile.out
+
+if [ "$HOST_PLATFORM" == "$target" ]; then
+  printf "  Running   ($target) CiRuntimeApi..."
+  $OUT/CiRuntimeApi &> $OUT/$target.run.out
+  check $?
+  
+fi
