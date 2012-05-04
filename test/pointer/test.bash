8ae1c14d84ee46c12a0cfaa293279b864c2f1b92
Ben L. Titzer <titzer@google.com>
2012-05-03 17:58:31 -0700

    Added tests for pointer closures and for array copy with pointers

--- test/pointer/test.bash
+++ test/pointer/test.bash
@@ -7,7 +7,12 @@ if [ "$TEST_TARGET" != $target ]; then
 	exit 0
 fi
 
-TESTS=*.v3
+if [ $# == 0 ]; then
+  TESTS=*.v3
+else
+  TESTS=$*
+fi
+
 printf "  Compiling ($target)..."
 mkdir -p $OUT/$target
 run_v3c "" -multiple -set-exec=false -target=$target-test -output=$OUT/$target $TESTS &> $OUT/compile.out
