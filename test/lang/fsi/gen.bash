174749009790111df96ce0c579ef7867dbdd9cd4
Ben L. Titzer <ben.titzer@gmail.com>
2013-02-22 13:02:03 -0800

    Add test cases for future language feature: fixed size integers

---
+++ test/lang/fsi/gen.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# gen all 1 <= L <= 64
+function gen_l() {
+  file=$1
+  l=1
+  while [ $l -lt 65 ]; do
+    sed "-es/\$L/$l/g" $file
+    l=$((l + 1))
+  done
+}
+
+# gen all (L, U) pairs where U > L
+function gen_lu() {
+  file=$1
+  l=1
+  while [ $l -lt 64 ]; do
+    u=$(($l + 1))
+    while [ $u -lt 64 ]; do
+      sed "-es/\$L/$l/g" $file | sed "-es/\$U/$u/g"
+      u=$((u + 1))
+    done
+    l=$((l + 1))
+  done
+}
+
+for f in $*; do
+    grep '\$U' $f &> /dev/null
+    if [ "$?" = "0" ]; then
+	echo $f;
+	gen_lu $f
+	continue
+    fi
+
+    grep '\$L' $f &> /dev/null
+    if [ "$?" = "0" ]; then
+	echo $f;
+	gen_l $f
+    fi
+done
