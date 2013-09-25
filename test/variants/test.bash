9659bb49ab82ff170da44fcbd5faccaed639b06c
Ben L. Titzer <titzer@google.com>
2013-09-25 22:37:04 +0200

    Fix type of first-class variant constructors for subsumption on JVM.

--- test/variants/test.bash
+++ test/variants/test.bash
@@ -6,12 +6,5 @@ if [ $# -gt 0 ]; then
 else
 	TESTS=*.v3
 fi
-
-if [ -z "$NATIVE" ]; then
-  run_int_tests "int" ""
-  run_int_tests "int-ra" "-ra"
-else
- run_exec_tests
-fi
-
+run_exec_tests
 exit $?
