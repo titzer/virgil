a29ff31aa3336866046e18b26d73b6aeae1b18dd
Ben L. Titzer <titzer@google.com>
2014-03-12 07:50:49 +0100

    Add cheesy DEPS files to applications to make them easier to build in an automated way.

--- test/apps/test.bash
+++ test/apps/test.bash
@@ -10,9 +10,15 @@ target=$TEST_TARGET
 
 function do_app() {
   printf "  Compiling ($target) %s..." $1
+  cd $1
   local out=$OUT/$1.compile.out
-  run_v3c $target -output=$OUT $1/*.v3 &> $out
+  local deps=""
+  if [ -f DEPS ]; then
+    deps=$(cat DEPS)
+  fi
+  run_v3c "$target" -output=$OUT *.v3 $deps &> $out
   check $?
+  cd ..
 }
 
 for b in $APPS; do
