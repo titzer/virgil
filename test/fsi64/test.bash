9c0b158abd31c132c17e0e885cc3e7fd38518c77
Ben L. Titzer <titzer@google.com>
2014-03-23 20:02:34 +0100

    Reshuffle fsi64 tests.

--- test/fsi64/test.bash
+++ test/fsi64/test.bash
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+function do_parser_tests() {
+	cd parser
+	printf "  Running parser tests..."
+	run_v3c "" -test -expect=expect.txt *.v3 > $OUT/out
+	check_red $OUT/out
+	cd ..
+}
+
+function do_seman_tests() {
+	cd seman
+	printf "  Running semantic tests..."
+	run_v3c "" -test -expect=expect.txt *.v3 > $OUT/out
+	check_red $OUT/out
+	cd ..
+}
+
 . ../common.bash fsi64
 if [ $# -gt 0 ]; then
 	TESTS="$@"
 else
+	(do_parser_tests)
+	(do_seman_tests)
+
 	TESTS=*.v3
 fi
 run_exec_tests
