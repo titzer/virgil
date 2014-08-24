068bb858928bb982b5febcda946e55ef075bf8a9
Ben L. Titzer <titzer@google.com>
2014-08-24 19:30:38 +0200

    Update btime to no longer need output files to be specified on the command line.

--- bench/btime.c
+++ bench/btime.c
@@ -12,8 +12,10 @@
 #include <errno.h>
 #include <math.h>
 
+#define MAX_RUNS 1000
+
 char **global_envp;
-double *times;
+double times[MAX_RUNS];
 
 // print timeval and rusage
 void print_time(struct timeval *elapsed, struct rusage *usage) {
@@ -31,30 +33,13 @@ void print_time(struct timeval *elapsed, struct rusage *usage) {
 }
 
 // run the program, redirecting its output to the given tmp files
-int run(int run, int numruns, char *tmp, char *argv[]) {
+int run(int run, int numruns, int nstdout, int nstderr, char *argv[]) {
   struct rusage usage;
   struct timeval start;
   struct timeval end;
   struct timeval elapsed;
   int pid, status = 0, i = 0, rc = 0;
-  int nstderr, nstdout;
   char tmpfname[1024];
-
-  // create stdout file
-  snprintf(tmpfname, 1024, "%s.%d.out", tmp, run);
-  nstdout = open(tmpfname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-  if (nstdout <= 0) {
-    printf("could not open output: %s\n", tmpfname);
-    return 1;
-  }
-
-  // create stderr file
-  snprintf(tmpfname, 1024, "%s.%d.err", tmp, run);
-  nstderr = open(tmpfname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-  if (nstderr <= 0) {
-    printf("could not open output: %s\n", tmpfname);
-    return 1;
-  }
 
   // get start time
   gettimeofday(&start, NULL);
@@ -69,7 +54,7 @@ int run(int run, int numruns, char *tmp, char *argv[]) {
     // execve the command
     status = execve(argv[0], argv, global_envp);
     // should not reach here unless exec failed
-    printf("status = %d, errno = %d\n", status, errno);
+    printf("exec failed with status = %d, errno = %d\n", status, errno);
     exit(status);
   }
 
@@ -77,18 +62,24 @@ int run(int run, int numruns, char *tmp, char *argv[]) {
   rc = wait4(pid, &status, 0, &usage);
   if (rc < 0 || !WIFEXITED(status) || WEXITSTATUS(status) != 0) {
     // program returned nonzero exit code, or terminated with signal
-    if (numruns > 1) printf(" %12s", "--");
-    else printf("failed with rc = %d, exited = %d, exit = %d\n", rc, WIFEXITED(status), WEXITSTATUS(status));
+    if (numruns > 1) {
+	printf(" %12s", "--");
+    } else if (rc == pid) {
+      if (WIFEXITED(status)) {
+        printf("process exited with rc = %d\n", WEXITSTATUS(status));
+      } else {
+	printf("process failed with status = %d\n", WEXITSTATUS(status));
+      }
+    } else {
+	printf("wait4 failed with rc = %d, exited = %d, exit = %d\n", rc, WIFEXITED(status), WEXITSTATUS(status));
+    }
     fflush(stdout);
     return 1;
   }
+
   // get elapsed time
   gettimeofday(&end, NULL);
   timersub(&end, &start, &elapsed);
-
-  // close output files
-  close(nstdout);
-  close(nstderr);
 
   times[run] = ((double) elapsed.tv_sec) + ((double) elapsed.tv_usec) / 1000000.0;
 
@@ -118,25 +109,40 @@ void print_stats(int runs) {
   printf("  avg=%.6lf  stddev=%.6lf", average, stddev);
 }
 
+int do_fail_run(char* argv[]) {
+  printf("\n=={ FAILED }=============================================\n");
+  run(0, 1, STDOUT_FILENO, STDERR_FILENO, argv);
+  printf(  "=========================================================\n");
+  return 1;
+}
+
 int main(int argc, char *argv[], char *envp[]) {
   int i;
-  char *tmp = argv[1];
+  char *tmp = "/dev/null";
   global_envp = envp;
-  if (argc < 3) {
-    printf("usage: btime <output> [runs] <command>\n");
+  if (argc < 2) {
+    printf("usage: btime [runs] <command>\n");
     exit(1);
   }
-  if (argc > 3) {
-    // could have specified the number of runs
-    char *num = argv[2];
+  // open /dev/null as the default output.
+  int devnull = open("/dev/null", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (devnull <= 0) {
+    printf("could not open /dev/null\n");
+    return 1;
+  }
+
+  if (argc > 2) {
+    // Check to see if the number of runs was specified.
+    char *num = argv[1];
     if (num[0] >= '0' && num[0] <= '9') {
       int runs = atoi(num);
+      if (runs > MAX_RUNS) runs = MAX_RUNS;
       if (runs > 1) {
 	int i = 0, failed = 0;
-        times = (double *) malloc(runs * sizeof(double));
         for(i = 0; i < runs; i++) {
-  	  if (failed) printf(" %12s", "--");
-	  else failed = (run(i, runs, tmp, &argv[3]) != 0);
+  	  if (run(i, runs, devnull, devnull, &argv[2]) != 0) {
+		return do_fail_run(&argv[2]);
+	  }
         }
         print_stats(runs);
         printf("\n");
@@ -144,6 +150,10 @@ int main(int argc, char *argv[], char *envp[]) {
       }
     }
   }
-  return run(0, 1, tmp, &argv[2]);
+  // Just do a single run.
+  if (run(0, 1, devnull, devnull, &argv[1]) != 0) {
+    return do_fail_run(&argv[1]);
+  }
+  return 0;
 }
 
