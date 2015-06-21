8225e49254b3fdeaf298efa3f4901ddef8e3e371
Ben L. Titzer <titzer@google.com>
2015-06-21 12:01:08 +0200

    Desugar enums into a single class.

---
+++ test/enums/test.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+jvm=0
+if [ "$1" == "-jvm" ]; then
+    jvm=1
+    shift
+fi
+
+function do_parser_tests() {
+	cd parser
+	printf "  Running parser tests..."
+	run_v3c "" -test -expect=expect.txt *.v3 > $OUT/out
+	check_red $OUT/out
+	cd ..
+}
+
+function do_seman_tests() {
+	cd seman
+	printf "  Running semantic tests..."
+	run_v3c "" -test -expect=expect.txt *.v3 > $OUT/out
+	check_red $OUT/out
+	cd ..
+}
+
+. ../common.bash enums
+if [ $# -gt 0 ]; then
+	TESTS="$@"
+else
+	(do_parser_tests)
+	(do_seman_tests)
+
+	TESTS=*.v3
+fi
+
+if [ "$jvm" == 1 ]; then
+  run_jvm_tests
+else
+  run_exec_tests
+fi
+exit $?
