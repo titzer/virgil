b200aaaa1d3278a576419776968d8315e205dd7f
Ben L. Titzer <ben.titzer@gmail.com>
2020-05-19 18:45:05 +1000

    Add test cases for ceil, floor, and sqrt

--- test/float/GenTestCase.java
+++ test/float/GenTestCase.java
@@ -3,18 +3,21 @@ import java.io.*;
 
 class GenTestCase {
     static PrintStream out = System.out;
-    static boolean ftrunc = false;
+    static boolean ftrunc = true;
     static String fsuffix = ftrunc ? "f" : "d";
+    static String template_name;
+    static String template;
 
     public static void main(String[] args) throws java.io.IOException {
-	String template_name = args[0];
-	String template = loadFile(template_name);
-
+	template_name = args[0];
+	template = loadFile(template_name);
+	genUnop();
+    }
+    static void genC() throws java.io.IOException {
 	int[] widths = {
 	    15, 16, 31, 32, 41, 52, 53, 63
 	};
-
-
+	
 	for (int width : widths) {
 	    for (boolean signed : new boolean[]{true, false}) {
 		IntType t = new IntType(signed, width);
@@ -44,6 +47,120 @@ class GenTestCase {
 
     }
 
+    static abstract class Unop {
+	boolean isDouble;
+	String ftype;
+	String itype;
+	String op;
+	Unop(boolean isDouble, String op) {
+	    this.isDouble = isDouble;
+	    this.ftype = isDouble ? "double" : "float";
+	    this.itype = isDouble ? "u64" : "u32";
+	    this.op = op;
+	}
+	abstract double exec(double d);
+    }
+    
+    static void genUnop() throws java.io.IOException {
+	for (boolean isDouble : new boolean[]{false, true}) {
+	    doUnop(new Unop(isDouble, "floor") {
+		    double exec(double d) {
+			return Math.floor(d);
+		    }
+		});
+	    doUnop(new Unop(isDouble, "ceil") {
+		    double exec(double d) {
+			return Math.ceil(d);
+		    }
+		});
+	    doUnop(new Unop(isDouble, "sqrt") {
+		    double exec(double d) {
+			return Math.sqrt(d);
+		    }
+		});
+	    doUnop(new Unop(isDouble, "abs") {
+		    double exec(double d) {
+			return Math.abs(d);
+		    }
+		});
+	}
+    }
+    
+    static void doUnop(Unop op) throws java.io.IOException {
+	double cases[] = {
+	    Double.NEGATIVE_INFINITY,
+	    Double.NaN,
+	    -134e16,
+	    -0xFFFFFFp-3,
+	    -0xFFFFFFp-2,
+	    -0xFFFFFFp-1,
+	    -0xFFFFFFp0,
+	    -0xFFFFFFp1,
+	    -0xFFFFFFp2,
+	    -0xFFFFFFp3,
+	    -3.9,
+	    -1.5,
+	    -1.25,
+	    -1,
+	    -1e-40,
+	    -0.125,
+	    -1e-319,
+	    -0,
+	    0,
+	    0.625,
+	    1e-320,
+	    1e-41,
+	    2.25,
+	    5.75,
+	    0xFFFFFFp-3,
+	    0xFFFFFFp-2,
+	    0xFFFFFFp-1,
+	    0xFFFFFFp0,
+	    0xFFFFFFp1,
+	    0xFFFFFFp2,
+	    0xFFFFFFp3,
+	    13455.16,
+	    124e17,
+	    Double.POSITIVE_INFINITY,
+	};
+	
+	ByteArrayOutputStream os = new ByteArrayOutputStream();
+	out = new PrintStream(os);
+	
+	for (double f : cases) {
+	    double val = f;
+	    if (!op.isDouble) val = (float)val;
+	    double result = op.exec(val);
+	    if (!op.isDouble) result = (float)result;
+	    out.println("\t(" + render(op.isDouble, val) + ", " + render(op.isDouble, result) + ", " + Double.isNaN(result) + "),");
+	}
+	out.println("\t(0, 0, false)");
+	    
+	String inputs = os.toString("UTF8");
+	String result = template
+	    .replaceAll("FTYPE", op.ftype)
+	    .replaceAll("ITYPE", op.itype)
+	    .replaceAll("OP", op.op)
+	    .replaceAll("INPUTS", inputs);
+	    
+	String outname = op.ftype +
+	    template_name
+	    .replaceAll("OP", op.op)
+	    .replaceAll(".t3", ".v3");
+	
+	out = new PrintStream(new FileOutputStream(outname));
+	out.println(result);
+	out.close();
+    }
+
+    static String render(boolean isDouble, double d) {
+	String suffix = isDouble ? "d" : "f";
+	if (d >= Float.POSITIVE_INFINITY) return "1e+2000" + suffix;
+	if (d <= Float.NEGATIVE_INFINITY) return "-1e+2000" + suffix;
+	if (Double.isNaN(d)) return isDouble ? "double.nan" : "float.nan";
+	return d + suffix;
+    }
+    
     private static String loadFile(String filePath) throws java.io.IOException {
 	StringBuilder contentBuilder = new StringBuilder();
 	try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
