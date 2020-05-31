554d3dc7158c6c4f161454bc79910afb0d9d060c
Ben L. Titzer <ben.titzer@gmail.com>
2020-05-31 22:11:53 +1000

    Add boundary condition tests for float/double roundi

--- test/float/strtod.c
+++ test/float/strtod.c
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
   for (int i = 1; i < argc; i++) {
     double d = strtod(argv[i], NULL);
     int64_t* p = (int64_t*)&d;
-    printf("\tassert_dv(\"%s\", 0x%016lx);\n", argv[i], *p);
+    printf("%s %lfd 0x%016lx\n", argv[i], d, *p);
   }
   return 0;
 }
