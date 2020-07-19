2c674dbe3512da23c4a644d67fcf05e88e3606fe
Ben L. Titzer <ben.titzer@gmail.com>
2020-07-19 22:23:12 +1000

    Assemble and test some SSE instructions

--- test/x86/asm/addsd.s
+++ test/x86/asm/addsd.s
@@ -1,25 +1,23 @@
+bits 32
 	addsd xmm0, xmm0
-	addsd xmm1, xmm0
-	addsd xmm2, xmm0
 	addsd xmm3, xmm0
-	addsd xmm4, xmm0
-	addsd xmm5, xmm0
-	addsd xmm6, xmm0
 	addsd xmm7, xmm0
 
-	addsd xmm0, xmm0
 	addsd xmm0, xmm1
-	addsd xmm0, xmm2
 	addsd xmm0, xmm3
-	addsd xmm0, xmm4
-	addsd xmm0, xmm5
-	addsd xmm0, xmm6
 	addsd xmm0, xmm7
 
 	addsd xmm2, xmm2
 	addsd xmm2, xmm3
 	addsd xmm3, xmm2
-	addsd xmm3, xmm3
-	addsd xmm3, xmm7
-	addsd xmm7, xmm3
 	
+	addsd xmm0, [eax]
+	addsd xmm7, [eax]
+	addsd xmm0, [ecx]
+	addsd xmm0, [esi]
+	addsd xmm0, [edi]
+	addsd xmm0, [esp]
+	addsd xmm0, [esp+8]
+
+	addsd xmm0, [0x99887766]
+	addsd xmm0, [0x77]
