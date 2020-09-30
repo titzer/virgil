e8c536f3d4c34f2e43967c5f3336e4be823241a1
Ben L. Titzer <ben.titzer@gmail.com>
2020-09-30 11:10:58 +1000

    More work on TypeRep

---
+++ apps/TypeRep/test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PROGRESS_PIPE=1
+if [ "$1" = "-fatal" ]; then
+    PROGRESS_PIPE=0
+fi
+
+v3c *.v3 $(cat DEPS)
+if [ "$?" -ne 0 ]; then
+    exit 1
+fi
+
+if [ "$PROGRESS_PIPE" = 1 ]; then
+    v3c -run *.v3 $(cat DEPS) $@ | progress i
+else
+    v3c -run *.v3 $(cat DEPS) $@
+fi
