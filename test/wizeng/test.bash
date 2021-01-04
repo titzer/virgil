94d34ab2e23006bca4879f43b89bc1dfa9cb30c1
Ben L. Titzer <ben.titzer@gmail.com>
2021-01-03 23:29:04 -0600

    Add lib/util functionality for Wizard

---
+++ test/wizeng/test.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. ../common.bash wizeng
+
+if [ "$WIZENG_LOC" = "" ]; then
+    printf "  WIZENG_LOC not set, ${YELLOW}skip${NORM}\n"
+    exit 0
+fi
+
+V3C=$AENEAS_TEST
+$WIZENG_LOC/test/unit.sh
