96763f86f0c44b237e9d4a3fe44328579ca1a83f
Ben L. Titzer <ben.titzer@gmail.com>
2021-01-04 18:54:12 -0600

    Fix and test Strings.parseLiteral

--- test/lib/test.bash
+++ test/lib/test.bash
@@ -2,14 +2,38 @@
 
 . ../common.bash lib
 
+let PROGRESS_PIPE=1
+if [[ "$1" =~ "-trace-calls=" ]]; then
+    V3C_OPTS="$1 $V3C_OPTS"
+    shift
+    let PROGRESS_PIPE=0
+fi
+
+if [[ "$1" =~ "-fatal-calls=" ]]; then
+    V3C_OPTS="$1 $V3C_OPTS"
+    shift
+    let PROGRESS_PIPE=0
+fi
+
+if [ $# -gt 0 ]; then
+  TESTS=$*
+else
+  TESTS=*.v3
+fi
+
 print_status Interpreting
 P=$OUT/run.out
-run_v3c "" *.v3 $VIRGIL_LOC/lib/util/*.v3
+run_v3c "" $TESTS $VIRGIL_LOC/lib/util/*.v3
 if [ "$?" != 0 ]; then
     printf "  %sfail%s: lib tests failed to compile\n" "$RED" "$NORM"
     exit 1
 fi
-run_v3c "" -run *.v3 $VIRGIL_LOC/lib/util/*.v3 | tee $P | $PROGRESS i
+
+if [ "$PROGRESS_PIPE" = 1 ]; then
+    run_v3c "" -run $TESTS $VIRGIL_LOC/lib/util/*.v3 | tee $P | $PROGRESS i
+else
+    run_v3c "" -run $TESTS $VIRGIL_LOC/lib/util/*.v3 | tee $P
+fi
 
 target=$TEST_TARGET
 
@@ -21,7 +45,7 @@ if [ "$target" != "" ]; then
     R=$OUT/$target/run.out
 
     print_compiling $target
-    run_v3c $target -output=$T *.v3 $VIRGIL_LOC/lib/util/*.v3 &> $C
+    run_v3c $target -output=$T $TESTS $VIRGIL_LOC/lib/util/*.v3 &> $C
     check_no_red $? $C
 
     print_status Running $target
