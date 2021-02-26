2ae27353af7ce46d208b8d27a2edf48dffab6519
Ben L. Titzer <ben.titzer@gmail.com>
2021-02-26 15:41:14 +0000

    x86-64: use opcode instead of compare size

---
+++ test/codegen-x86-64.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# XXX: compile aeneas to tmp directory
+aeneas bootstrap
+
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+DIRS="execute fsi32 fsi64 legacy_intcast future_intcast pointer variants enums large float"
+
+if [ $# != 0 ]; then
+    DIRS="$@"
+fi
+
+for d in $DIRS; do
+    echo --- $d ---------
+    (cd $d; RUN_WASM=0 RUN_JVM=0 RUN_INT=0 RUN_NATIVE=0 RUN_X86_64=1 ./test.bash)
+done
