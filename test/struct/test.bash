6603b69317893b619265dad15f3de23e830a1ba6
Miss-Bamboo <yunqingy@andrew.cmu.edu>
2022-07-13 20:31:51 -0400

    Started DSL project

---
+++ test/struct/test.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function do_parser_tests() {
+    cd parser
+    print_status Parser ""
+    run_v3c "" -test -expect=expect.txt *.v3 | tee $OUT/out | $PROGRESS i
+    cd ..
+}
+
+function do_seman_tests() {
+    cd seman
+    print_status Semantic ""
+    run_v3c "" -test -expect=expect.txt *.v3 | tee $OUT/out | $PROGRESS i
+    cd ..
+}
+
+. ../common.bash struct
+if [ $# -gt 0 ]; then
+	TESTS="$@"
+else
+	(do_parser_tests)
+	(do_seman_tests)
+
+	TESTS=*.v3
+fi
+
+execute_tests
+exit $?
