78f00a3fab133ee075aef59bb1cc0931c19a9ce3
Kunal Sareen <kunal.sareen@anu.edu.au>
2022-12-01 17:12:04 +1100

    Add simple test CI
    This commit adds a simple CI that tests the following targets using
    GitHub actions:
    
     - {x86,x86_64}-linux
     - {x86_64}-macos
     - jvm

---
+++ ci/macos/build-test-x86_64.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+source "${DIR}/../../test/common.bash"
+
+${VIRGIL_LOC}/test/configure
+
+TEST_TARGETS="int x86-64-macos"
+aeneas test
