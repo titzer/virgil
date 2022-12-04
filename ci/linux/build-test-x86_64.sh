fd6ac58a7d0914956a19df2b5133736a8754dca7
Kunal Sareen <kunal.sareen@anu.edu.au>
2022-12-05 03:14:46 +1100

    Install nasm in CI for the x86 Linux targets

--- ci/linux/build-test-x86_64.sh
+++ ci/linux/build-test-x86_64.sh
@@ -6,6 +6,9 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 VIRGIL_LOC="${DIR}/../.."
 TEST_DIR="${VIRGIL_LOC}/test"
 
+echo "Install nasm"
+sudo apt -y install nasm
+
 "${TEST_DIR}"/configure
 
 PROGRESS_ARGS=l TEST_TARGETS="int x86-64-linux" "${TEST_DIR}"/all.bash
