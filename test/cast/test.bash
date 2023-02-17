fb7cbc073854782189512399a2a6b651ba461987
Ben L. Titzer <ben.titzer@gmail.com>
2023-02-17 11:13:32 -0500

    [test] Migrate some legacy_cast tests to cast

--- test/cast/test.bash
+++ test/cast/test.bash
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+function do_seman_tests() {
+    cd seman
+    print_status Semantic ""
+    run_v3c "" -test -expect=failures.txt *.v3 | tee $OUT/out | $PROGRESS
+    cd ..
+}
+
 . ../common.bash cast
 if [ $# -gt 0 ]; then
 	TESTS="$@"
 else
+	(do_seman_tests)
+
 	TESTS=*.v3
 fi
+
 execute_tests
 exit $?
