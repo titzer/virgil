c4740c6e6f5dfd733bc47c6835075d783fb12837
Ben L. Titzer <ben.titzer@gmail.com>
2023-06-21 10:46:30 -0400

    [ci] Add scripts for different optimization levels of Wasm

---
+++ ci/wasm/build-test-wasm-O2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+VIRGIL_LOC="${DIR}/../.."
+TEST_DIR="${VIRGIL_LOC}/test"
+
+"${TEST_DIR}"/configure
+
+V3C_OPTS=-O2 PROGRESS_ARGS=c TEST_TARGETS="wasm-js" "${TEST_DIR}"/all.bash
