b6402c4a0d776d96a5e212e8191a1dbdbc73fbff
Ben L. Titzer <ben.titzer@gmail.com>
2023-11-10 11:39:10 -0500

    [rt] Add -ir-alloc option which implements .alloc stub in SSA

--- test/pointer/test.bash
+++ test/pointer/test.bash
@@ -16,6 +16,8 @@ for target in $TEST_TARGETS; do
 	continue # skip because not native
     elif [ "$target" = wasm-js ]; then
 	TESTS=$(ls $TEST_LIST | grep -v _64.v3)
+    elif [ "$target" = wasm-spec ]; then
+	TESTS=$(ls $TEST_LIST | grep -v _64.v3)
     elif [ "$target" = x86-linux ]; then
 	TESTS=$(ls $TEST_LIST | grep -v _64.v3)
     elif [ "$target" = x86-darwin ]; then
