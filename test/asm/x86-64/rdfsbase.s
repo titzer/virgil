79d29b3b6fe4b3352e308a5afb5a2c96b2e2ea7a
Ben L. Titzer <ben.titzer@gmail.com>
2023-11-15 13:13:48 -0500

    [lib/asm] Add {rd,wr}{f,g}sbase instructions

---
+++ test/asm/x86-64/rdfsbase.s
@@ -0,0 +1,25 @@
+bits 64
+
+	rdfsbase rax
+	rdfsbase rbx
+	rdfsbase rcx
+	rdfsbase rdx
+	rdfsbase rsi
+	rdfsbase rdi
+	rdfsbase rbp
+	rdfsbase rsp
+	rdfsbase r10
+	rdfsbase r11
+	rdfsbase r12
+	rdfsbase r13
+	rdfsbase r14
+	rdfsbase r15
+	
+	rdgsbase rax
+	rdgsbase r10
+	
+	wrfsbase rdx
+	wrfsbase r12
+	
+	wrgsbase rcx
+	wrgsbase r13
