781c1fba0796e6bca4a217b1bcca065cc8c1080d
Ben L. Titzer <ben.titzer@gmail.com>
2023-12-21 19:17:55 -0500

    Fix mapBlockEnd bug

--- test/gc/test.bash
+++ test/gc/test.bash
@@ -29,7 +29,7 @@ function do_int_test() {
     rm -f $ALL
 
     if [[ "$target" =~ "x86-64" ]]; then
-        HEAP='-heap-size=65m' # 64-bit needs more heap
+        HEAP='-heap-size=66m' # 64-bit needs more heap
     else
         HEAP='-heap-size=32m'
     fi
