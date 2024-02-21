5ca92d774468a01a3f7482feb8cf1e55dc964892
Ben L. Titzer <ben.titzer@gmail.com>
2024-02-21 14:54:01 -0500

    [packing] Add example of CiRuntime packings

--- bin/virgil-mode.el
+++ bin/virgil-mode.el
@@ -16,12 +16,12 @@
 
 (setq virgil-font-lock-keywords
   `(
+    (,"#unboxed" . font-lock-type-face)
+    (,"#boxed" . font-lock-type-face)
+    (,"#packing" . font-lock-type-face)
     (,virgil-decls-regexp . font-lock-keyword-face)
     (,virgil-stmts-regexp . font-lock-constant-face)
     (,virgil-builtins-regexp . font-lock-type-face)
-    (,"#unboxed" . font-lock-type-face)
-    (,"#boxed" . font-lock-type-face)
-    (,"#packed" . font-lock-type-face)
 ))
 
 (setq indent-tabs-mode t)
