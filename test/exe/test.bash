a089ef34d59c61afb77b59c3a479241562f04ef8
Ben L. Titzer <ben.titzer@gmail.com>
2024-03-18 15:13:56 -0400

    WIP run more exe tests in parallel

--- test/exe/test.bash
+++ test/exe/test.bash
@@ -9,21 +9,24 @@ cd $VIRGIL_TEST/exe/
 DIRS="core variants"
 
 # parallel tests
-function compile_all_targets() {
+function do_parallel() {
+    action=$1
+    shift
     MAX=16
     i=3
     for target in $TEST_TARGETS; do
-	if [ -x $S/compile-exe-${target}.bash ]; then
+	if [ -x $S/${action}-exe-${target}.bash ]; then
 	    FIFO=$OUT/$target.compile.fifo
 	    F=$OUT/$i.fifo
 	    rm -f $FIFO $F
 	    mkfifo $FIFO
 	    ln -s $FIFO $F
-	    $S/compile-exe-jvm.bash "$@" > $FIFO &
+	    $S/${action}-exe-${target}.bash "$@" > $FIFO &
 	    i=$(($i + 1))
 	fi
     done
-
+    n=$(($i - 3))
+    
     # pad the rest of the fifos
     while [ $i -le $MAX ]; do
 	F=$OUT/$i.fifo
@@ -33,10 +36,30 @@ function compile_all_targets() {
     done
     
     # yuck! stupid bash.
-    progress p 3<$OUT/3.fifo 4<$OUT/4.fifo 5<$OUT/5.fifo 6<$OUT/6.fifo 7<$OUT/7.fifo 8<$OUT/8.fifo 9<$OUT/9.fifo 10<$OUT/10.fifo 11<$OUT/11.fifo 12<$OUT/12.fifo 13<$OUT/13.fifo 14<$OUT/14.fifo 15<$OUT/15.fifo 16<$OUT/16.fifo
+    printf "Running ${action} ($n processes): "
+    progress ip 3<$OUT/3.fifo 4<$OUT/4.fifo 5<$OUT/5.fifo 6<$OUT/6.fifo 7<$OUT/7.fifo 8<$OUT/8.fifo 9<$OUT/9.fifo 10<$OUT/10.fifo 11<$OUT/11.fifo 12<$OUT/12.fifo 13<$OUT/13.fifo 14<$OUT/14.fifo 15<$OUT/15.fifo 16<$OUT/16.fifo
 }
 
-compile_all_targets ../core/*.v3
+function do_serial() {
+    action=$1
+    shift
+    printf "Running ${action} (serially)   : "
+    for target in $TEST_TARGETS; do
+	if [ -x $S/${action}-exe-${target}.bash ]; then
+	    $S/${action}-exe-${target}.bash "$@"
+	fi
+    done | progress i
+}
+
+for dir in core cast variants enums fsi32 fsi64 float range layout large; do
+#    echo "---- $dir ------------------------"
+    time do_parallel compile ../$dir/*.v3
+#    time do_serial compile ../$dir/*.v3
+
+    time do_parallel run ../$dir/*.v3
+#    time do_serial run ../$dir/*.v3
+done
+
 
 exit 0
 
---
+++ test/scripts/run-exe-x86-64-linux.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export VIRGIL_TEST=$(builtin cd $(dirname ${BASH_SOURCE[0]}) && builtin cd .. && builtin pwd)
+
+. $VIRGIL_TEST/scripts/run-exe-common.bash x86-64-linux "$@"
