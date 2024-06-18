50895e24007399573a49b11679f263b95805bab6
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-06-17 23:28:53 -0400

    Add start of count-vowels
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- apps/Extism/build.sh
+++ apps/Extism/build.sh
@@ -6,3 +6,11 @@ v3c \
   -output=greet \
   -target=wasm \
   greet/Greet.v3 $(cat DEPS)
+
+v3c \
+  -entry-export=_initialize \
+  -heap-size=500m \
+  -main-export=_initialize \
+  -output=count-vowels \
+  -target=wasm \
+  count-vowels/CountVowels.v3 $(cat DEPS)
