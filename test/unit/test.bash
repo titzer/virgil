4e556aa8d2b744bfed0bca2810e17cd06efa07aa
davidmaamoaix <davidmaamoaix@gmail.com>
2024-12-09 00:13:36 -0500

    unit test passing

--- test/unit/test.bash
+++ test/unit/test.bash
@@ -6,7 +6,6 @@ printf "  Running Aeneas unit tests..."
 P=$OUT/block_order.out
 pushd $VIRGIL_LOC > /dev/null
 SRCS="aeneas/src/*/*.v3 $(cat aeneas/DEPS)"
-run_v3c "" -fp -run $SRCS $AENEAS_LOC/../test/*.v3 -version
-# run_v3c "" -fp -run $SRCS $AENEAS_LOC/../test/*.v3 -version | $PROGRESS
-# popd > /dev/null
+run_v3c "" -fp -run $SRCS $AENEAS_LOC/../test/*.v3 -version | $PROGRESS
+popd > /dev/null
 
