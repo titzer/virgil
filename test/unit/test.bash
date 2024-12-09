a4384fc7f9d1a1e09bd4e2a3d7ca0c80ae6612a4
davidmaamoaix <davidmaamoaix@gmail.com>
2024-12-09 00:03:13 -0500

    parse error detection

--- test/unit/test.bash
+++ test/unit/test.bash
@@ -6,6 +6,7 @@ printf "  Running Aeneas unit tests..."
 P=$OUT/block_order.out
 pushd $VIRGIL_LOC > /dev/null
 SRCS="aeneas/src/*/*.v3 $(cat aeneas/DEPS)"
-run_v3c "" -fp -run $SRCS $AENEAS_LOC/../test/*.v3 -version | $PROGRESS
-popd > /dev/null
+run_v3c "" -fp -run $SRCS $AENEAS_LOC/../test/*.v3 -version
+# run_v3c "" -fp -run $SRCS $AENEAS_LOC/../test/*.v3 -version | $PROGRESS
+# popd > /dev/null
 
