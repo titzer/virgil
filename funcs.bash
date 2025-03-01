24a5a2cd437f11dbfc1538d44c7af34e4c48d613
Eliot Moss <moss@cs.umass.edu>
2025-03-01 16:57:43 -0500

    Improvements that overcome some failures in my setup

--- funcs.bash
+++ funcs.bash
@@ -3,15 +3,22 @@ function cd {
 	# this guards against:
 	# 1) user definitions of cd that may print stuff, etc.
 	# 2) paths not starting in / that match in user's CDPATH
+	if [ x-P = "x$1" ]; then poption="-P"; shift; fi
 	local target="$1"
 	if [ -n "${target##/*}" ]; then
 		# add ./ if path does not start with /
 		target="./$target"
 	fi
 	# use builtin to avoid user-overloaded cd
-	builtin cd "$target"
+	builtin cd $poption "$target"
 }
 function pwd {
 	# this guards against user definitons of pwd that act differently
 	builtin pwd
+}
+function pushd {
+	builtin pushd $*
+}
+function popd {
+	builtin popd
 }
