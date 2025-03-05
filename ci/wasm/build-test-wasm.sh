f4a10ac5f531e1c941577ebd44ac653377e49fa0
Eliot Moss <moss@cs.umass.edu>
2025-03-05 18:08:31 -0500

    Changes to make scripts more robust to users changing cd, etc.
    Getting right collection of stuff into this PR

--- ci/wasm/build-test-wasm.sh
+++ ci/wasm/build-test-wasm.sh
@@ -2,11 +2,12 @@
 SOURCE=${BASH_SOURCE[0]}
 HERE=$(command dirname $SOURCE)
 . $HERE/funcs.bash
-DIR=$(cd -P $HERE && pwd)
+# set DIR to true location of this file
+follow_links $SOURCE
 
 VIRGIL_LOC=$DIR/../..
 TEST_DIR=$VIRGIL_LOC/test
 
 $TEST_DIR/configure
 
-V3C_OPTS="$@" PROGRESS_ARGS=c TEST_TARGETS="wasm" $TEST_DIR/all.bash
+V3C_OPTS="$@" PROGRESS_ARGS=c TEST_TARGETS="wasm-js" $TEST_DIR/all.bash
