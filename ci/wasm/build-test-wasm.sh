7d1f2ebbceee75f07b3adcb05026f9928c1b3606
Eliot Moss <moss@cs.umass.edu>
2025-03-05 21:29:06 -0500

    A ci file needs a slight change - it references a non-existent target

--- ci/wasm/build-test-wasm.sh
+++ ci/wasm/build-test-wasm.sh
@@ -2,12 +2,11 @@
 SOURCE=${BASH_SOURCE[0]}
 HERE=$(command dirname $SOURCE)
 . $HERE/funcs.bash
-# set DIR to true location of this file
-follow_links $SOURCE
+DIR=$(cd -P $HERE && pwd)
 
 VIRGIL_LOC=$DIR/../..
 TEST_DIR=$VIRGIL_LOC/test
 
 $TEST_DIR/configure
 
-V3C_OPTS="$@" PROGRESS_ARGS=c TEST_TARGETS="wasm-js" $TEST_DIR/all.bash
+V3C_OPTS="$@" PROGRESS_ARGS=c TEST_TARGETS="wasm" $TEST_DIR/all.bash
