51d7b1c4d1af5b3307c99224b78a98e55c6d7270
Eliot Moss <moss@cs.umass.edu>
2025-04-27 22:59:06 -0400

    This gets much further along in the wasm gc backend work. It includes a few test-related files that will eventually go away.

--- test/wasmgc/test.bash
+++ test/wasmgc/test.bash
@@ -1,15 +1,48 @@
 #!/bin/bash
 
 . ../common.bash wasmgc
-if [ $# -gt 0 ]; then
-	TESTS="$@"
-else
-	do_parser_tests
-	do_seman_tests
 
-	TESTS=*.v3
+if [ $# -gt 0 ]; then
+    TEST_LIST="$@"
+else
+
+#   do_parser_tests
+#   do_seman_tests
+
+    TEST_LIST=*.v3
 fi
 
-execute_tests
+LAST=0
+for target in $TEST_TARGETS; do
+    if [ "$target" = v3i ]; then
+        TESTS=$(ls $TEST_LIST | grep -v _pn)
+#	continue # skip because not native target
+    elif [[ "$target" = jvm || "$target" = jar ]]; then
+        TESTS=$(ls $TEST_LIST | grep -v _pn)
+#	continue # skip because not native
+    elif [ "$target" = wasm ]; then
+        TESTS=$(ls $TEST_LIST)
+    elif [ "$target" = wasm-gc ]; then
+        TESTS=$(ls $TEST_LIST)
+    elif [ "$target" = x86-linux ]; then
+        TESTS=$(ls $TEST_LIST | grep -v _pn | grep -v _64)
+        TESTS=$(ls $TESTS | grep -v ranges) # temporary, because of reg alloc issue
+    elif [ "$target" = x86-darwin ]; then :
+        TESTS=$(ls $TEST_LIST | grep -v _pn | grep -v _64)
+        TESTS=$(ls $TESTS | grep -v ranges) # temporary, because of reg alloc issue
+    elif [ "$target" = x86-64-linux ]; then
+        TESTS=$(ls $TEST_LIST | grep -v _pn)
+    elif [ "$target" = x86-64-darwin ]; then
+        TESTS=$(ls $TEST_LIST | grep -v _pn)
+    else
+	continue;
+    fi
 
-exit 0
+    if [ -n "$TESTS" ]; then
+        compile_target_tests $target
+        execute_target_tests $target
+        LAST=$?
+    fi
+done
+
+exit $LAST
