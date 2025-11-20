6f1e6bfa1823b88679697e69bf8e7043a45a94b5
Daniel Nguyen <dnguyen2@andrew.cmu.edu>
2025-11-20 01:52:25 -0500

    [licm] Fixed CI workflow script. hyperfine installation and fixed paths

--- ci/linux/licm-microbench.sh
+++ ci/linux/licm-microbench.sh
@@ -5,45 +5,74 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 VIRGIL_LOC="${DIR}/../.."
 TEST_DIR="${VIRGIL_LOC}/apps/MicroLICM"
 
+# Check necessary commands are installed
 if [ "$(type -t nasm)" = "" ]; then
     echo "Install nasm"
     sudo apt -y install nasm
 fi
+if [ "$(type -t hyperfine)" = "" ]; then
+    echo "Install hyperfine"
+    sudo apt -y install hyperfine 
+fi
+
 TMP_DIR="/tmp/licm-microbench"
 
-if [ -f "${TMP_DIR}/default/licm-microbench" ]; then 
+# Remove old executables 
+if [ -d "${TMP_DIR}/default/licm-microbench" ]; then 
     rm "${TMP_DIR}/default/licm-microbench"
 fi
 
-if [ -f "${TMP_DIR}/licm/licm-microbench" ]; then 
+if [ -d "${TMP_DIR}/licm/licm-microbench" ]; then 
     rm "${TMP_DIR}/licm/licm-microbench"
 fi
 
-if [ -f "${TMP_DIR}/results.json" ]; then 
+if [ -e "${TMP_DIR}/results.json" ]; then 
     rm "${TMP_DIR}/results.json"
 fi
+echo "Directories cleaned"
 
 mkdir -p "${TMP_DIR}"
 mkdir -p "${TMP_DIR}/default"
 mkdir -p "${TMP_DIR}/licm"
 
-echo "$@ -output=${TMP_DIR}/default"
 
-v3c-x86-64-linux $@ -O2 -output=${TMP_DIR}/default/ ${TEST_DIR}/licm-microbench.v3
-v3c-x86-64-linux $@ -O2 -licm -output=${TMP_DIR}/licm/ ${TEST_DIR}/licm-microbench.v3
+compile() {
+    local runLICM=$1
+    local dest=$2
+    if [[ -e "${VIRGIL_LOC}/bin/v3c-x86-64-linux" ]]; then 
+        echo "Found current Aeneas"
+        CURRENT="${VIRGIL_LOC}/bin/v3c-x86-64-linux"
+        if [ $runLICM = "true" ]; then 
+            echo "Compiling with LICM | Executable destination: ${dest}"
+            $CURRENT -licm -output=${dest} ${TEST_DIR}/licm-microbench.v3
+        else 
+            echo "Compiling without LICM | Executable destination: ${dest}"
+            $CURRENT -output=${dest} ${TEST_DIR}/licm-microbench.v3
+        fi
+    else 
+        echo "Current Aeneas not found"
+        $DEV clean
+        exit 1
+    fi
+}
+
+echo "Compiling"
+DEV="${VIRGIL_LOC}/bin/dev/aeneas"
+$DEV clean
+compile "false" "${TMP_DIR}/default/"
+$DEV bootstrap
+compile "true" "${TMP_DIR}/licm/"
+$DEV clean
 
 hyperfine --export-json ${TMP_DIR}/results.json "${TMP_DIR}/default/licm-microbench" "${TMP_DIR}/licm/licm-microbench" --runs=3
 
 DEFAULT_MEAN=$(jq -r '.results[0].mean' ${TMP_DIR}/results.json)
 LICM_MEAN=$(jq -r '.results[1].mean' ${TMP_DIR}/results.json)
 
-echo "DEFAULT_MEAN ${DEFAULT_MEAN}"
-echo "LICM_MEAN ${LICM_MEAN}"
-
 if [[ ${DEFAULT_MEAN} > ${LICM_MEAN} ]]; then 
-    echo "Default mean, ${DEFAULT_MEAN}, is larger than LICM mean, ${LICM_MEAN}."
+    echo "Success! | Default mean, ${DEFAULT_MEAN}, is larger than LICM mean, ${LICM_MEAN}."
     exit 0
 else 
-    echo "LICM mean, ${LICM_MEAN}, is larger than Default mean, ${DEFAULT_MEAN}."
+    echo "Failure | LICM mean, ${LICM_MEAN}, is larger than Default mean, ${DEFAULT_MEAN}."
     exit 1    
 fi
