898abc5f1e0ec503d886d63e01511c7199248673
Ben L. Titzer <ben.titzer@gmail.com>
2025-12-17 11:53:18 -0500

    [test] Support -fatal for unit tests (#536)

--- test/unit/test.bash
+++ test/unit/test.bash
@@ -2,10 +2,14 @@
 
 . ../common.bash aeneas
 
+if [ "$1" = "-fatal" ]; then
+    FATAL="-fatal"
+fi
+
 printf "  Running Aeneas unit tests..."
 P=$OUT/block_order.out
 pushd $VIRGIL_LOC > /dev/null
 SRCS="aeneas/src/*/*.v3 $(cat aeneas/DEPS)"
-run_v3c "" -fp -run $SRCS $AENEAS_LOC/../test/*.v3 -version | $PROGRESS
+run_v3c "" -fp -run $SRCS $AENEAS_LOC/../test/*.v3 $FATAL -version | $PROGRESS
 popd > /dev/null
 
