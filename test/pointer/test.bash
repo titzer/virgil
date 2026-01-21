bd48b627a97d5a8d2b485b1c2e99700d76543841
Ben L. Titzer <ben.titzer@gmail.com>
2026-01-21 18:25:39 -0500

    Added filtration of failing tests per platform

--- test/pointer/test.bash
+++ test/pointer/test.bash
@@ -12,22 +12,30 @@ fi
 
 LAST=0
 for target in $TEST_TARGETS; do
+    # filter out any expected failures for the target
+    expected="failures.$target"
+    if [ -f "$expected" ]; then
+	TESTS=$(ls $TEST_LIST | grep -v -f "$expected")
+    else
+	TESTS=$TEST_LIST
+    fi
+    
     if [ "$target" = v3i ]; then
 	continue # skip because not native target
     elif [[ "$target" = jvm || "$target" = jar ]]; then
 	continue # skip because not native
     elif [ "$target" = wasm ]; then
-	TESTS=$(ls $TEST_LIST | grep -v _64.v3)
+	TESTS=$(ls $TESTS | grep -v _64.v3)
     elif [ "$target" = wasm-gc ]; then
         continue # skip because only certain operations work
     elif [ "$target" = x86-linux ]; then
-	TESTS=$(ls $TEST_LIST | grep -v _64.v3)
+	TESTS=$(ls $TESTS | grep -v _64.v3)
     elif [ "$target" = x86-darwin ]; then
-	TESTS=$(ls $TEST_LIST | grep -v _64.v3)
+	TESTS=$(ls $TESTS | grep -v _64.v3)
     elif [ "$target" = x86-64-linux ]; then
-	TESTS=$(ls $TEST_LIST | grep -v _32.v3)
+	TESTS=$(ls $TESTS | grep -v _32.v3)
     elif [ "$target" = x86-64-darwin ]; then
-	TESTS=$(ls $TEST_LIST | grep -v _32.v3)
+	TESTS=$(ls $TESTS | grep -v _32.v3)
     else
 	continue;
     fi
